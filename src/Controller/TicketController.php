8c81f43159371a8edea998a5aa184b19a76e4fd2
Guilherme Freire Hack <hidan.akatsu@gmail.com>
2016-04-23 15:04:22 -0300

    Turn off debug

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Ticket Controller
+ *
+ * @property \App\Model\Table\TicketTable $Ticket
+ */
+class TicketController extends AppController {
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function index() {
+    }
+}
